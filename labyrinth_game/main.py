b03921e1e183af79cdcb4da0e74f305735c0b25d
Maksim Staroverov <mstaroverov6@gmail.com>
2025-09-27 22:16:33 +0300

    Добавляем функции и файлы для игры

--- labyrinth_game/main.py
+++ labyrinth_game/main.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
+from labyrinth_game.constants import ROOMS
 
+game_state = {
+    'player_inventory': [], # Инвентарь игрока
+    'current_room': 'entrance', # Текущая комната
+    'game_over': False, # Значения окончания игры
+    'steps_taken': 0 # Количество шагов
+}
 def main():
-    print("Первая попытка запустить проект!")
+    print("Добро пожаловать в Лабиринт!")
+    print(ROOMS[game_state['current_room']]['description'])
 
 if __name__ == "__main__":
     main()
+  
